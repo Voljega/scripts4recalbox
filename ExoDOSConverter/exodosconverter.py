bb23012a5112d18ee2a9b1064b32cc1b4594cdf1
Voljega <voljega@gmail.com>
2017-10-19 14:03:14 +0200

    bugs correction

--- ExoDOSConverter/exodosconverter.py
+++ ExoDOSConverter/exodosconverter.py
@@ -17,52 +17,108 @@ def isGoodLine(l,cLines):
             return False    
     return True
 
-def reducePath(path,game):
+def reducePath(path,game):    
     if path.startswith(".\games") or path.startswith("\games") or path.startswith("games") :
-        pathList = path.split('\\')
-        print(pathList)
+        pathList = path.split('\\')        
         if pathList[0]=='.' :
             pathList = pathList[1:]        
-        if pathList[0].lower()=='games' and pathList[1].lower()==game.lower() :
+        if len(pathList) > 1 and pathList[0].lower()=='games' and pathList[1].lower()==game.lower() :
             cleanedPath = "."
             for pathElt in pathList[2:] :
-                cleanedPath = cleanedPath + "\\" + pathElt
-            print("cleanedPath = "+cleanedPath)
+                cleanedPath = cleanedPath + "\\" + pathElt            
             return cleanedPath
         else :
             return path
     else :
         return path
-        
 
-def handleCDMount(line,game) :
-    command = line.split(" ")
+
+def cleanCue(old,new,cleanName):
+    oldFile = open(old,'r')
+    newFile = open(new,'w')   
+    for line in oldFile.readlines() :        
+        if line.startswith("FILE") :
+            params = line.split('"')
+            params[1] = cleanName + "." + params[1].split(".")[-1].lower()
+            line = '"'.join(params)
+            print("cue FILE line -> " +line.rstrip('\n\r'))
+            
+        newFile.write(line)
+    oldFile.close()
+    newFile.close()
+  
+def cleanCDname(path,game,dest):
+    pathFile = os.path.join(dest,path)    
+    if os.path.exists(pathFile) :
+        if os.path.isdir(pathFile) :            
+            return path
+        else :                     
+            pathList = path.split('\\')
+            filename = pathList[-1]                       
+            cleanName = filename.split(".")[0].lower()
+            ext = filename.split(".")[-1]
+            if len(cleanName)>8 :
+                cleanName = cleanName[0:7]            
+            dirPath = "\\".join(pathFile.split('\\')[:-1])
+            for file in os.listdir(dirPath) :                
+                if len(file.split("."))>1 and file.split(".")[1].lower() == "cue" :
+                    print("create new clean cue file "+cleanName+"."+file.split(".")[-1])
+                    sourceFile = file;
+                    targetFile = cleanName+"."+file.split(".")[-1].lower();
+                    source = os.path.join(dirPath,sourceFile)
+                    target = os.path.join(dirPath,targetFile)
+#                    print("%s != %s %r" %(sourceFile.lower(),targetFile,not sourceFile.lower() == targetFile))
+#                    print("%s == %s %r" %(sourceFile.lower(),targetFile, sourceFile.lower() == targetFile))
+                    if not sourceFile.lower() == targetFile :
+                        cleanCue(source,target,cleanName)
+                        os.remove(os.path.join(dirPath,file))
+                    else :
+                        print ("cue already well named")
+                        cleanCue(source,target+"1",cleanName)
+                        os.remove(os.path.join(dirPath,file))
+                        os.rename(target+"1",target)
+                elif file.split(".")[0].lower() == filename.split(".")[0].lower() :
+                    print("renamed %s to %s" %(file,cleanName+"."+file.split(".")[-1]))
+                    #double rename to avoid problems of same name with different case
+                    os.rename(os.path.join(dirPath,file),os.path.join(dirPath,cleanName+"."+file.split(".")[-1].lower()+"1"))
+                    os.rename(os.path.join(dirPath,cleanName+"."+file.split(".")[-1].lower()+"1"),os.path.join(dirPath,cleanName+"."+file.split(".")[-1].lower()))
+                
+            cleanedPath = "\\".join(pathList[:-1])+"\\"+cleanName+"."+ext.lower()
+            print("modify dosbox.bat : %s -> %s" %(path,cleanedPath))
+            return cleanedPath
+    else :
+        print("<ERROR> %s doesn't exist" %pathFile)
+        return path
+    
+    
+
+def handleCDMount(line,game,dest) :    
+    line = line.replace("@","")#always show imgmount command    
+    command = line.split(" ")    
     startIndex = -1
     endIndex = -1
     count =0
     for param in command :
-        if param == 'd' :
+        if param == 'd' or param == 'a' or param == 'b':
             startIndex = count
         elif param == '-t' :
             endIndex = count
         count = count + 1
     
     paths = command[startIndex+1:endIndex]
-    prString = "["+str(startIndex)+","+str(endIndex)+"] "
+    prString = ""
     for path in paths :
         path = reducePath(path.replace('"',""),game)        
-        filename = path.split(".")[0]        
-        if len(filename)>8 :
-            print("WARNING non-DOS filename %s in imgmount" %filename)
-        else :
-            print("DOS filename %s in imgmount" %filename)
+        if len(paths)==1 :
+            #HANDLE FILENAME FOR LEN(PATHS ) == 1
+            path = cleanCDname(path,game,dest)        
         prString = prString + " "+path
     
-    print("imgtomount "+prString)
-    return line
+    fullString = " ".join(command[0:startIndex+1]) + prString + " " + " ".join(command[endIndex:])    
+    return fullString
 
-def createDosboxBat(lines,dbB,dbCCfg,game) :
-    gameDosDir = os.path.join(exoDosDir,"Games","!dos",game)
+def createDosboxBat(lines,dbB,dbCCfg,game,dest) :
+    gameDir = os.path.join(exoDosDir,"Games",game)
     #nécessaire de voir où est monté mount c ?
     cutLines = ["cd ..","@cd ..","cls","mount c","@mount c","#","exit"]
     for line in lines :
@@ -72,15 +128,19 @@ def createDosboxBat(lines,dbB,dbCCfg,game) :
             #remove cd to gamedir            
             if line.lower().startswith("cd ") or line.lower().startswith("@cd") :                
                 path = reducePath(line.rstrip('\n\r ').split(" ")[-1].rstrip('\n\r '),game)                
-#                print("<%s> <%s>" %(path,game))
-#                print("? %s -> %r" %(os.path.join(gameDosDir,path),os.path.exists(os.path.join(gameDosDir,path))))
-                if path.lower() == game.lower() and not os.path.exists(os.path.join(gameDosDir,path)):
+#                print("<%s> <%s>" %(path,game))                
+#                print("? %s -> %r" %(os.path.join(gameDir,path),os.path.exists(os.path.join(gameDir,path))))
+                if path.lower() == game.lower() and not os.path.exists(os.path.join(gameDir,path)):
                     print("analyzing cd path %s -> path is game name and no existing subpath, removed" %line.rstrip('\n\r '))
                 else :
                     print("analyzing cd path %s -> kept" %line.rstrip('\n\r '))
                     dbB.write(line)
             elif line.lower().startswith("@imgmount d") or line.lower().startswith("imgmount d"):
-                dbB.write(handleCDMount(line,game))
+                dbB.write(handleCDMount(line,game,dest))
+                dbB.write("pause\n")
+            elif line.lower().startswith("mount ") or line.lower().startswith("@mount "):
+                dbB.write(handleCDMount(line,game,dest))
+                dbB.write("pause\n")
             else :
                 dbB.write(line)       
     #insérer pause
@@ -94,7 +154,9 @@ def convertConfiguration(game,genre) :
     count =0
     lines = dbC.readlines()
     for line in lines :
-        if line.startswith("fullresolution"):
+        if line.startswith("fullscreen"):
+            dbCCfg.write("fullscreen=true\n")
+        elif line.startswith("fullresolution"):
             dbCCfg.write("fullresolution=desktop\n")
         elif line.startswith("output"):
             dbCCfg.write("output=texture\n")
@@ -107,7 +169,7 @@ def convertConfiguration(game,genre) :
             dbCCfg.write("\n")
         elif line.startswith("[autoexec]"):
             dbCCfg.write(line)            
-            createDosboxBat(lines[count+1:],dbB,dbCCfg,game)
+            createDosboxBat(lines[count+1:],dbB,dbCCfg,game,dest)
             break
         else :
             dbCCfg.write(line)
@@ -123,6 +185,7 @@ def handleMetadata(game,gamesDosDir) :
     meagreDir = os.path.join(gamesDosDir,game,"Meagre")
     manualDir = os.path.join(meagreDir,"Manual")
     picDir = os.path.join(meagreDir,"Front")
+    screenPicDir = os.path.join(meagreDir,"Screen")
     aboutFile = os.path.join(meagreDir,"About","about.txt")
     iniFileDir = os.path.join(meagreDir,"IniFile")
     
@@ -136,6 +199,7 @@ def handleMetadata(game,gamesDosDir) :
         if key == "Name" :
             name = confLine[1].rstrip('\n\r ')
             safeEscapedName = name.replace(":","")
+            safeEscapedName = safeEscapedName.replace("/","-")
         elif key == "Genre" :
             genre = confLine[1].rstrip('\n\r ')
         elif key == "SubGenre" :
@@ -148,15 +212,22 @@ def handleMetadata(game,gamesDosDir) :
             year = confLine[1].rstrip('\n\r ')
         elif key == "Front01" :
             frontPic = confLine[1].rstrip('\n\r ')
+        elif key == "Screen01" :
+            screenPic = confLine[1].rstrip('\n\r ')
     #RECUPERER SCREEN SI FRONT N'EXISTE PAS
     
     front = os.path.join(picDir,frontPic)
-    #copy front pic
+    screen = os.path.join(screenPicDir,screenPic)
+    #copy front pic if exists else screenPic
     if not os.path.exists(os.path.join(outputDir,"downloaded_images")) :
         os.mkdir(os.path.join(outputDir,"downloaded_images"))
     if os.path.exists(front) and not os.path.isdir(front):
         print("copy pic file %s to %s" %(frontPic,os.path.join(outputDir,"downloaded_images",safeEscapedName +" - front.jpg")))
         shutil.copy2(front,os.path.join(outputDir,"downloaded_images",safeEscapedName +" - front.jpg"))
+        frontPic = "./downloaded_images/"+safeEscapedName +" - front.jpg"
+    elif os.path.exists(screen) and not os.path.isdir(screen):
+        print("copy pic file %s to %s" %(screenPic,os.path.join(outputDir,"downloaded_images",safeEscapedName +" - front.jpg")))
+        shutil.copy2(screen,os.path.join(outputDir,"downloaded_images",safeEscapedName +" - front.jpg"))
         frontPic = "./downloaded_images/"+safeEscapedName +" - front.jpg"
     #copy manual files
     if not os.path.exists(os.path.join(outputDir,"manuals")) :
@@ -175,10 +246,9 @@ def handleMetadata(game,gamesDosDir) :
                 
     dosGame = DosGame(name,genre,subgenre,publisher,developer,year,"./downloaded_images/"+safeEscapedName +" - front.jpg",about)
     print("")
-    print("Metadata for %s : %s (%s)" %(game,dosGame.name,dosGame.year))
-    print("genre: %s , subgenre: %s" %(dosGame.genre,dosGame.subgenre))
-    print("publisher: %s , developer: %s" %(dosGame.publisher,dosGame.developer))
-    print("pic : %s" %dosGame.frontPic)    
+    print("Metadata for %s : %s (%s), genre: %s , subgenre: %s" %(game,dosGame.name,dosGame.year,dosGame.genre,dosGame.subgenre))    
+#    print("publisher: %s , developer: %s" %(dosGame.publisher,dosGame.developer))
+#    print("pic : %s" %dosGame.frontPic)    
     print("")
     return dosGame
 
@@ -202,16 +272,38 @@ def writeGamelistEntry(gamelist,dosGame,game,genre):
     gamelist.write("    </game>\n")
 
 def buildGenre(dosGame):
-    if dosGame.genre in ['Adventure','Simulation','Sports']:
+    if dosGame.genre in ['Adventure','Sports']:
         return dosGame.genre
-    elif dosGame.genre == 'Strategy':
+    elif "Racing" in dosGame.genre :
+        return "Race"
+    elif dosGame.genre == 'Strategy' and "Board" in dosGame.subgenre:
+        return 'Puzzle'
+    elif dosGame.genre == 'Strategy' and not "Puzzle" in dosGame.subgenre:
         return 'Strategy-Gestion pre-1990' if int(dosGame.year) < 1990 else 'Strategy-Gestion'
+    elif dosGame.genre == 'Strategy' and "Puzzle" in dosGame.subgenre:
+        return "Puzzle"
+    elif dosGame.genre == 'Simulation' and dosGame.subgenre == 'Managerial' :
+        return 'Strategy-Gestion pre-1990' if int(dosGame.year) < 1990 else 'Strategy-Gestion'
+#    elif dosGame.genre == 'Simulation' and dosGame.subgenre == 'Strategy' :
+#        return 'Strategy-Gestion pre-1990' if int(dosGame.year) < 1990 else 'Strategy-Gestion'
+    elif dosGame.genre == 'Simulation' and dosGame.subgenre == 'Sports' :
+        return 'Sports'
+    elif dosGame.genre == 'Simulation' :
+        return 'Simulation'
     elif dosGame.genre == 'RPG':
         return 'RPG pre-1990' if int(dosGame.year) < 1990 else 'RPG'
     elif dosGame.genre == 'Action' and dosGame.subgenre == 'Pinball':
         return 'Pinball'
     elif dosGame.genre == 'Action' and dosGame.subgenre == 'Shooter':
         return 'ShootEmUp'
+    elif dosGame.genre == 'Action' and dosGame.subgenre == 'Platform':
+        return 'Platform'
+    elif dosGame.genre == 'Action' and 'FPS' in dosGame.subgenre :
+        return 'Gun-FPS'
+    elif dosGame.genre == 'Action' and dosGame.subgenre == 'Fighting':
+        return 'BeatEmUp'
+    elif dosGame.genre == 'Action' :
+        return 'Action-Adventure'
     else :
         return 'Unknown'
 
@@ -244,6 +336,7 @@ def convert(nbGames):
         
         dosGame = handleMetadata(game,gamesDosDir)
         genre = buildGenre(dosGame)
+        print("stuffed into genre %s" %genre)
         writeGamelistEntry(gamelist,dosGame,game,genre)
         
         if not os.path.exists(os.path.join(outputDir,genre,game+".pc")):
@@ -255,13 +348,6 @@ def convert(nbGames):
     
     gamelist.write("</gameList>\n")
     gamelist.close()
-    
-#♥3b3b/convertir path & cd dans le dosbox.bat    
-#    4/ convertir tous les paths en version recalbox (. pointe vers l'intérieur du répertoire du jeu)
-#    5/ si cdrom a gérer avec plus de 8 lettres, le renommer en moins, éventuellement .cue à éditer égalementt
-#    7/ déplacer le dossier de conf dosbox de !dos dans !dos-converted
-# bug sur cd 1stPerso qui devrait être enlevé
-    
 
 if __name__ == "__main__":
     nbGames = int(input("How many games do you want to convert ? : ").lower())
